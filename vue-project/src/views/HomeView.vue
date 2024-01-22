a787bad9e01dd8e0c2c901e11413bfe00e9dafb5
Minh Quan Tran <62203526+MinhQuan-Tran@users.noreply.github.com>
2024-01-22 17:12:03 +0700

    Start

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <img class="landing-img" src="@/assets/RE4wppc.jpg" alt="" />
+    <a class="explore-btn" href="weather.html">
+      <span class="the-arrow -left">
+        <span class="shaft"></span>
+      </span>
+      <span class="main">
+        <span class="text"> Explore More </span>
+        <span class="the-arrow -right">
+          <span class="shaft"></span>
+        </span>
+      </span>
+    </a>
+  </main>
+</template>
