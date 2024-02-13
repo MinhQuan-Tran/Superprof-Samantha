8996f48460244da5c995eb7962cd7f2a1d550baa
Minh Quan Tran <62203526+MinhQuan-Tran@users.noreply.github.com>
2024-02-13 07:22:27 +0700

    Make Gallery

--- vue-project/src/views/GalleryView.vue
+++ vue-project/src/views/GalleryView.vue
@@ -1,9 +1,25 @@
-<script setup></script>
+<script setup>
+import '@shoelace-style/shoelace/dist/components/carousel/carousel.js';
+
+import { ref } from 'vue';
+
+const carousel = ref(null);
+</script>
 
 <template>
-    <main id="gallery-trails">
-        <h1>This is a Gallery page</h1>
+    <main id="gallery">
+        <sl-carousel ref="carousel">
+            <sl-carousel-item v-for="index in 11" :key="index" @click="carousel.goToSlide(index - 1, smooth)">
+                <img :src="`/images/gallery/${index + 20}.png`" :alt="`${index + 20}.png`">
+            </sl-carousel-item>
+        </sl-carousel>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+sl-carousel {
+    --scroll-hint: 10%;
+}
+
+/* TODO: make prev and next item smaller */
+</style>
